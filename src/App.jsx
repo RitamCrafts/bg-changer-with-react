fe28c306b6c5f1056a6b3707cb59d9078d923bcf
RitamCrafts <ritamrm1157@gmail.com>
2026-05-06 22:48:24 +0530

    work-on responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <div className="h-screen w-screen" style={{
       backgroundColor: colour
     }}>  
-        <div className="colour-container flex justify-between items-stretch gap-1.5 bottom-3 absolute w-fit bg-white rounded-full h-10 left-1/2 -translate-1/2 p-2 shadow-2xl border-blue-500 border">
+        <div className="colour-container grid grid-cols-4 sm:flex sm:justify-between items-stretch gap-3 sm:gap-1.5 bottom-6 absolute w-70 sm:w-fit bg-white rounded-xl sm:rounded-full h-fit auto-rows-[30px] sm:h-10 left-1/2 -translate-x-1/2 p-2 shadow-2xl border-blue-500 border">
           <button onClick={()=>setColour("red")} className="outline-none colour w-15 bg-[red] rounded-full active:outline hover:opacity-55" />
           <button onClick={()=>setColour('blue')} className="outline-none colour w-15 bg-[blue] rounded-full active:outline hover:opacity-55" />
           <button onClick={()=>setColour('green')} className="outline-none colour w-15 bg-[green] rounded-full active:outline hover:opacity-55" />
@@ -15,7 +15,12 @@ function App() {
           <button onClick={()=>setColour('yellow')} className="outline-none colour w-15 bg-[yellow] rounded-full active:outline hover:opacity-55" />
           <button onClick={()=>setColour('orange')} className="outline-none colour w-15 bg-[orange] rounded-full active:outline hover:opacity-55" /> 
           <button onClick={()=>setColour('pink')} className="outline-none colour w-15 bg-[pink] rounded-full active:outline hover:opacity-55" /> 
+          <button onClick={()=>setColour('purple')} className="outline-none colour w-15 bg-[purple] rounded-full active:outline hover:opacity-55" /> 
+
         </div>
+
+        
+      
       
     </div>
   )
